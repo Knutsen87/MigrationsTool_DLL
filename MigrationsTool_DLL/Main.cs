3569370f19b0d1b9c710b11b62a50859f4ed41b0
Denni <Denni@DESKTOP-26IOATB>
2021-09-27 16:01:31 +0200

    Projektdateien hinzufügen.

---
+++ MigrationsTool_DLL/Main.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MigrationsTool_DLL
+{
+    public class Main
+    {
+        string FilePath = "";
+        public void ()
+        {
+            using(StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+
+            string path = @"";
+            if(!File.Exits(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine();
+                }
+            }
+        }
+    }
+}
